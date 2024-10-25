606e1c150f9609e45209db94ce679494aaeddfde
egaprasetyo <dionisius.ega26@gmail.com>
2024-10-26 02:21:06 +0700

    slicing find kos

---
+++ index.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next-custom",
+        prevEl: ".swiper-button-prev-custom",
+    },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", "sans-serif"],
+            },
+            backgroundImage:{
+                'hero-image': "linear-gradient(to right, black, transparent), url('/assets/img/webp/modern-architecture.webp')",
+                'overlay-gradient-card': 'linear-gradient(180deg, rgba(255, 255, 255, 0) 46%, rgba(5, 2, 17, 1) 87%)',
+                'overlay-gradient-card-wide': 'linear-gradient(180deg, rgba(255, 255, 255, 0) 30%, rgba(5, 2, 17, 1) 100%)',
+            }
+        },
+    },
+};
