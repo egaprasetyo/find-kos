f36a58806d5503fbb3fda48435e601ed61a95efc
egaprasetyo <dionisius.ega26@gmail.com>
2024-10-26 02:25:35 +0700

    fix path assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ tailwind.config = {
                 sans: ["Poppins", "sans-serif"],
             },
             backgroundImage:{
-                'hero-image': "linear-gradient(to right, black, transparent), url('/assets/img/webp/modern-architecture.webp')",
+                'hero-image': "linear-gradient(to right, black, transparent), url('assets/img/webp/modern-architecture.webp')",
                 'overlay-gradient-card': 'linear-gradient(180deg, rgba(255, 255, 255, 0) 46%, rgba(5, 2, 17, 1) 87%)',
                 'overlay-gradient-card-wide': 'linear-gradient(180deg, rgba(255, 255, 255, 0) 30%, rgba(5, 2, 17, 1) 100%)',
             }
